6dd39c961ad09661f4eb12dfd2f0f9c19c1e13c6
janthipx <janthipa2006@gmail.com>
2025-09-24 09:17:47 +0700

    Update ConcreteMission.ts

--- ConcreteMission.ts
+++ ConcreteMission.ts
@@ -1,29 +1,13 @@
-import { Mission } from "./Final/interface Misssion";
-import { Leader } from "../Leader";
+import { Mission } from "./Final/Misssion";
+import { Leader } from "./Leader";
 import { Member } from "./Final/Member";
 import { MissionStatus } from "./Final/enum";
 
-export class ConcreteMission implements Mission {
-  public members: Member[] = [];
-  public status: MissionStatus = MissionStatus.NotStarted;
+export interface ConcreteMission extends Mission {
+  id: number;
+  name: string;
+  leader: Leader;
+  members: Member[];
+  status: MissionStatus;
 
-  constructor(public id: number, public name: string, public leader: Leader) {}
-
-  startMission(): void {
-    this.status = MissionStatus.InProgress;
-  }
-
-  finishMission(): void {
-    this.status = MissionStatus.Finished;
-  }
-
-  addMember(member: Member): void {
-    if (!this.members.includes(member)) {
-      this.members.push(member);
-    }
-  }
-
-  removeMember(member: Member): void {
-    this.members = this.members.filter((m) => m.id !== member.id);
-  }
 }
