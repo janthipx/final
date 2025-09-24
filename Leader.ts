c8b666f5557bbce146741efd43120cb90f9897c4
janthipx <janthipa2006@gmail.com>
2025-09-24 07:39:18 +0700

    Create Leader.ts

---
+++ Leader.ts
@@ -0,0 +1,19 @@
+import { Mission } from "./Final/interface Misssion";
+
+export class Leader {
+  constructor(public id: number, public name: string) {}
+
+  startMission(mission: Mission): string {
+    mission.startMission();
+    return `Leader ${this.name} started mission ${mission.name}`;
+  }
+
+  endMission(mission: Mission): string {
+    mission.finishMission();
+    return `Leader ${this.name} ended mission ${mission.name}`;
+  }
+
+  announceResults(mission: Mission): string {
+    return `Results announced for mission ${mission.name}`;
+  }
+}
