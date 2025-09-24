2f41522c68c356d3b8001bcd09a2e4d8562b2859
janthipx <janthipa2006@gmail.com>
2025-09-24 09:18:14 +0700

    Update Database.ts

--- Database.ts
+++ Database.ts
@@ -1,9 +1,12 @@
-
-import { Mission } from "./Final/interface Misssion";
+import { Mission } from "./Final/Misssion";
 import { Member } from "./Final/Member";
+import { Score } from "./Score";
+
+
 export class Database {
   private missions: Mission[] = [];
   private members: Member[] = [];
+  public scores: Score[] = [];
 
   saveMission(mission: Mission): void {
     this.missions.push(mission);
@@ -13,11 +16,19 @@ export class Database {
     this.members.push(member);
   }
 
+  saveScore(score: Score): void {
+    this.scores.push(score);
+  }
+
   fetchMission(id: number): Mission | null {
     return this.missions.find((m) => m.id === id) || null;
   }
-
   fetchMember(id: number): Member | null {
     return this.members.find((m) => m.id === id) || null;
   }
+
+  fetchScoresByMember(memberId: number): Score[] {
+    return this.scores.filter((s) => s.memberId === memberId);
+  }
 }
+
