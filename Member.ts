ce42b8d68fff24160b5ae4727bc2a64ffd370fe2
janthipx <janthipa2006@gmail.com>
2025-09-24 07:37:53 +0700

    Create Member.ts

---
+++ Member.ts
@@ -0,0 +1,20 @@
+import { Mission } from "./interface Misssion";
+import { MemberStatus } from "./enum";
+
+export class Member {
+  constructor(
+    public id: number,
+    public name: string,
+    public status: MemberStatus = MemberStatus.Inactive
+  ) {}
+
+  joinMission(mission: Mission): void {
+    mission.addMember(this);
+    this.status = MemberStatus.Joined;
+  }
+
+  leaveMission(mission: Mission): void {
+    mission.removeMember(this);
+    this.status = MemberStatus.Left;
+  }
+}
