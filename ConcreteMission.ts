e256a3befd89d76702bdb91eb7424d36b428100b
janthipx <janthipa2006@gmail.com>
2025-09-28 21:42:34 +0700

    Update ConcreteMission.ts

--- ConcreteMission.ts
+++ ConcreteMission.ts
@@ -1,11 +1,1 @@
-import { Mission } from "./Final/Misssion";
-import { Leader } from "./Leader";
-import { Member } from "./Final/Member";
-import { MissionStatus } from "./Final/enum";
 
-export interface ConcreteMission extends Mission {
-  id: number;
-  name: string;
-  leader: Leader;
-  
-}
